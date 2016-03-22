23d85bb9f3a8e6714fce8ebb1df13dfcb0c5e44b
Tony Su <peitaosu@163.com>
2016-03-22 22:28:44 +0800

    add simple listener() class, override listener() functions

--- motion-presentation/motion_pre_leap.py
+++ motion-presentation/motion_pre_leap.py
@@ -1,4 +1,4 @@
-import os, sys, time, inspect
+import os, sys, time, inspect, datetime
 
 src_dir = os.path.dirname(inspect.getfile(inspect.currentframe()))
 arch_dir = '../../motion-leap/lib/x64' if sys.maxsize > 2 ** 32 else '../../motion-leap/lib/x86'
@@ -7,9 +7,24 @@ sys.path.insert(0, os.path.abspath(os.path.join(src_dir, arch_dir)))
 sys.path.insert(0, os.path.abspath(os.path.join(src_dir, leap_dir)))
 import Leap
 
+class Listener(Leap.Listener):
+    def on_init(self, controller):
+        print str(datetime.datetime.now())+" Initialized"
+
+    def on_connect(self, controller):
+        print str(datetime.datetime.now())+" Connected"
+
+    def on_disconnect(self, controller):
+        print str(datetime.datetime.now())+" Disconnected"
+
+    def on_exit(self, controller):
+        print str(datetime.datetime.now())+" Exited"
+
+    def on_frame(self, controller):
+	print str(datetime.datetime.now())+" "+str(controller.frame().current_frames_per_second)
 
 def main():
-    listener = Leap.Listener()
+    listener = Listener()
     controller = Leap.Controller()
     controller.add_listener(listener)
 
